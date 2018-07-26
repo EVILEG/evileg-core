f6372e9433bf3edd7599119ca768fed9b2951a23
Evgenii Legotckoi <legotckoi@yandex.ru>
2018-07-26 20:17:26 +0200

    Move get_object_or_none to ESNF-C

---
+++ shortcuts.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+def get_object_or_none(klass, *args, **kwargs):
+    try:
+        return klass._default_manager.get(*args, **kwargs)
+    except klass.DoesNotExist:
+        return None
