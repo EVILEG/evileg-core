bb97266d2b85cc324d2d71d1ad73dd940e7c99f0
Evgeni Legotckoi <legotckoi@yandex.ru>
2018-07-31 23:19:12 +0200

    ESNF-C: Added EModerationMixin, EAbstractModeratedPost, EModerationMixinAdmin, EPostModeratedAdmin

--- evileg_core/admin.py
+++ evileg_core/admin.py
@@ -1,9 +1,63 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
 
 class EPostAdmin(admin.ModelAdmin):
-    list_display = ('content', 'author', 'pub_date', 'lastmod')
+    list_display = ('__str__', 'author', 'pub_date', 'lastmod')
     autocomplete_fields = ['author']
     search_fields = ('content', 'author__username')
+
+
+class EModerationMixinAdmin(admin.ModelAdmin):
+    list_display = ('moderation',)
+    list_filter = ('moderation',)
+    actions = ['make_spam', 'make_not_moderated', 'make_post_moderated', 'make_moderated']
+
+    def moderate(self, request, rows_updated, choice_description):
+        if rows_updated == 1:
+            message_bit = "1 entry is marked as %s" % choice_description
+        else:
+            message_bit = "%s entries are marked as %s." % (rows_updated, choice_description)
+        self.message_user(request, "%s" % message_bit)
+
+    def make_spam(self, request, queryset):
+        self.moderate(
+            request=request,
+            rows_updated=queryset.update(moderation=self.model.SPAM),
+            choice_description=_("SPAM")
+        )
+
+    make_spam.short_description = _("Set marked as SPAM")
+
+    def make_not_moderated(self, request, queryset):
+        self.moderate(
+            request=request,
+            rows_updated=queryset.update(moderation=self.model.NOT_MODERATED),
+            choice_description=_("NOT MODERATED")
+        )
+
+    make_not_moderated.short_description = _("Set marked as NOT MODERATED")
+
+    def make_post_moderated(self, request, queryset):
+        self.moderate(
+            request=request,
+            rows_updated=queryset.update(moderation=self.model.POST_MODERATED),
+            choice_description=_("POST MODERATED")
+        )
+
+    make_post_moderated.short_description = _("Set marked as POST MODERATED")
+
+    def make_moderated(self, request, queryset):
+        self.moderate(
+            request=request,
+            rows_updated=queryset.update(moderation=self.model.MODERATED),
+            choice_description=_("MODERATED")
+        )
+
+    make_moderated.short_description = _("Set marked as MODERATED")
+
+
+class EPostModeratedAdmin(EModerationMixinAdmin, EPostAdmin):
+    list_display = EPostAdmin.list_display + EModerationMixinAdmin.list_display
--- evileg_core/models.py
+++ evileg_core/models.py
@@ -7,10 +7,41 @@ from django.utils.translation import ugettext_lazy as _
 
 class EAbstractPost(models.Model):
 
-    class Meta:
-        abstract = True
-
     author = models.ForeignKey(User, verbose_name=_("Author"), on_delete=models.CASCADE)
     content = models.TextField(_('Content'), blank=True)
     pub_date = models.DateTimeField(_('Publication date'), blank=True, null=True)
     lastmod = models.DateTimeField(_('Last modification date'), blank=True, null=True)
+
+    def __str__(self):
+        return self.content[:150]
+
+    class Meta:
+        abstract = True
+
+
+class EModerationMixin(models.Model):
+
+    SPAM = 'S'
+    NOT_MODERATED = 'N'
+    POST_MODERATED = 'P'
+    MODERATED = 'M'
+    MODERATION_CHOICES = (
+        (SPAM, _('SPAM')),
+        (NOT_MODERATED, _('Not Moderated')),
+        (POST_MODERATED, _('Post Moderated')),
+        (MODERATED, _('Moderated'))
+    )
+
+    moderation = models.CharField(
+        _('Moderation'),
+        max_length=1,
+        choices=MODERATION_CHOICES,
+        default=NOT_MODERATED
+    )
+
+    class Meta:
+        abstract = True
+
+
+class EAbstractModeratedPost(EModerationMixin, EAbstractPost):
+    pass
