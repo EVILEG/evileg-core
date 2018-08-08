b58f996bbd2ca05f58a26478e36626c63f692c2d
Evgeni Legotckoi <legotckoi@yandex.ru>
2018-08-08 18:51:59 +0200

    ESNF: bootstrap-material-design.css/js to evileg_core.css/js

--- evileg_core/templatetags/evileg_core.py
+++ evileg_core/templatetags/evileg_core.py
@@ -2,6 +2,7 @@
 
 from django import template
 from django.contrib.contenttypes.models import ContentType
+from django.templatetags.static import static
 
 register = template.Library()
 
@@ -11,3 +12,23 @@ def activities_count(activity_set, model_name):
     if model_name:
         return activity_set.search(model=ContentType.objects.get(model=model_name).model_class()).count()
     return activity_set.count()
+
+
+@register.simple_tag
+def evileg_core_css():
+    return static("css/evileg_core.css")
+
+
+@register.simple_tag
+def evileg_core_min_css():
+    return static("css/evileg_core.min.css")
+
+
+@register.simple_tag
+def evileg_core_js():
+    return static("css/evileg_core.js")
+
+
+@register.simple_tag
+def evileg_core_min_js():
+    return static("css/evileg_core.min.js")
