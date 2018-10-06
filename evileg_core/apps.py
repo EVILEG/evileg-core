f2cd39683139060d72d397e546365132132542a8
Evgeni Legotckoi <legotckoi@yandex.ru>
2018-10-06 17:41:43 +0200

    ESNF: Update verbose_name, verbose_name_plural ESNF-C: Update verbose_name, verbose_name_plural

--- evileg_core/apps.py
+++ evileg_core/apps.py
@@ -1,5 +1,9 @@
+# -*- coding: utf-8 -*-
+
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class EvilegCoreConfig(AppConfig):
     name = 'evileg_core'
+    verbose_name = _('EVILEG Core')
