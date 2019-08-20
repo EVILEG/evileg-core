a0449f00b103129646f52f68da93de8c843252c9
Evgenii Legotckoi <legotckoi@yandex.ru>
2019-08-20 20:42:00 +0200

    ESNF-C: Bugfix: If one user has email like username of another user, we must check all opportunities

--- evileg_core/backends.py
+++ evileg_core/backends.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 from django.db.models import Q
-
 from evileg_core.shortcuts import get_object_or_none
 
 
@@ -10,15 +11,35 @@ class EEmailOrUsernameModelBackend:
     """
     Authentication backend for using email or username for authentication on the site.
     """
+
+    def validate_email(self, email):
+        """
+        Check if email is really email
+
+        :param email: email
+        :return: email or None
+        """
+        try:
+            validate_email(email)
+            return email
+        except ValidationError:
+            return None
+
     def authenticate(self, request, username=None, password=None):
         """
 
         :param request: HTTP Request
-        :param username: username of email
+        :param username: username or email
         :param password: password
         :return: User Object or None
         """
-        user = get_object_or_none(get_user_model(), Q(username__exact=username) | Q(email__exact=username))
+        email = self.validate_email(username)
+        if email:
+            user = get_object_or_none(get_user_model(), Q(email__exact=username))
+            if user and user.check_password(password):
+                return user
+
+        user = get_object_or_none(get_user_model(), Q(username__exact=username))
         return user if user and user.check_password(password) else None
 
     def get_user(self, user_id):
