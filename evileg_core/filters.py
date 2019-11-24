d47b5d323bfc0a5a96f6016f1e2aa0c7bdd395f8
Legotckoi <legotckoi@yandex.ru>
2019-11-24 13:50:41 +0100

    ESNF-C: move EFilterBase, EArticleFilter, ESectionFilter to evileg_core

--- evileg_core/filters.py
+++ evileg_core/filters.py
@@ -2,11 +2,12 @@
 
 import datetime
 
+import django_filters
 from django.contrib import admin
 from django.contrib.admin.filters import FieldListFilter
-from django.db.models import DateField
-from django.db.models import Q
+from django.db.models import DateField, Q
 from django.utils.translation import ugettext_lazy as _
+from django_filters import filters
 
 from .forms import EDateRangeForm
 
@@ -108,3 +109,62 @@ class EExactEmptyFilter(admin.SimpleListFilter):
         if self.value() == 'null':
             is_null_true = {filter_string: ''}
             return queryset.filter(Q(**is_null_true))
+
+
+class EFilterBase(django_filters.FilterSet):
+    advanced_search = False
+
+    search = filters.CharFilter(field_name='search', method='main_search')
+
+    class Meta:
+        fields = ['search']
+
+    def main_search(self, qs, name, value):
+        return qs.filter(Q(title__icontains=value) | Q(content__icontains=value))
+
+    @classmethod
+    def create(cls, target_model):
+        class FilterBase(EFilterBase):
+            class Meta(EFilterBase.Meta):
+                model = target_model
+        return FilterBase
+
+
+class EArticleFilter(EFilterBase):
+    advanced_search = True
+
+    ordering_by_title = filters.OrderingFilter(choices=(
+        ('title', _('Title')),
+        ('-title', _('Title (descending)')),
+    ), label=_('Sort by title'))
+    ordering_by_views = filters.OrderingFilter(choices=(
+        ('-views', _('Views')),
+        ('views', _('Views (descending)'))
+    ), label=_('Sort by views'))
+    ordering_by_pub_date = filters.OrderingFilter(choices=(
+        ('pub_date', _('Publication date')),
+        ('-pub_date', _('Publication date (descending)')),
+    ), label=_('Sort by publication date'))
+
+    @classmethod
+    def create(cls, target_model):
+        class ArticleFilter(EArticleFilter):
+            class Meta(EArticleFilter.Meta):
+                model = target_model
+        return ArticleFilter
+
+
+class ESectionFilter(EFilterBase):
+    advanced_search = True
+
+    ordering_by_title = filters.OrderingFilter(choices=(
+        ('title', _('Title')),
+        ('-title', _('Title (descending)')),
+    ), label=_('Sort by title'))
+
+    @classmethod
+    def create(cls, target_model):
+        class SectionFilter(ESectionFilter):
+            class Meta(ESectionFilter.Meta):
+                model = target_model
+        return SectionFilter
