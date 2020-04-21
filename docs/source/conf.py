f83a004ef5a05671ae51402069b0b62bf97c01f0
Legotckoi <legotckoi@yandex.ru>
2020-04-21 22:13:20 +0200

    ESNF-С: Return to read the docs theme, removed ads

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -82,7 +82,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -105,7 +105,6 @@ html_static_path = ['_static']
 #
 # html_sidebars = {}
 
-html_style = 'css/evileg.css'
 
 # -- Options for HTMLHelp output ---------------------------------------------
 
