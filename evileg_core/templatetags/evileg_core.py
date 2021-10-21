9959517db750cf0131b60e666b3a74f9a086f812
Legotckoi <legotckoi@yandex.ru>
2021-10-21 20:47:30 +0200

    remove customization of scrollbar

--- evileg_core/templatetags/evileg_core.py
+++ evileg_core/templatetags/evileg_core.py
@@ -16,7 +16,7 @@ from ..json_ld import generate_site_navigation_element_json_ld
 register = template.Library()
 
 
-STATIC_CONTENT_VERSION = 65
+STATIC_CONTENT_VERSION = 66
 
 CLASSIC = 1
 DARCULA = 2
