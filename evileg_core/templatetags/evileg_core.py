8db91317221036484a873832d219561559d1eb4a
Legotckoi <legotckoi@yandex.ru>
2023-03-24 22:31:55 +0100

    Improvement of navbar

--- evileg_core/templatetags/evileg_core.py
+++ evileg_core/templatetags/evileg_core.py
@@ -16,7 +16,7 @@ from ..json_ld import generate_site_navigation_element_json_ld
 register = template.Library()
 
 
-STATIC_CONTENT_VERSION = 86
+STATIC_CONTENT_VERSION = 87
 
 CLASSIC = 1
 DARCULA = 2
