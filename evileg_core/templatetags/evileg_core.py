a00ca0692e7d7e1f0e8d148bb938327c0b333c91
Legotckoi <legotckoi@yandex.ru>
2023-03-24 20:59:09 +0100

    Refactoring object headers

--- evileg_core/templatetags/evileg_core.py
+++ evileg_core/templatetags/evileg_core.py
@@ -16,7 +16,7 @@ from ..json_ld import generate_site_navigation_element_json_ld
 register = template.Library()
 
 
-STATIC_CONTENT_VERSION = 85
+STATIC_CONTENT_VERSION = 86
 
 CLASSIC = 1
 DARCULA = 2
