a91d25698715768da2cfe95580ed391cfdb62605
Legotckoi <legotckoi@yandex.ru>
2023-04-19 21:27:30 +0200

    Improvement of ads top

--- evileg_core/templatetags/evileg_core.py
+++ evileg_core/templatetags/evileg_core.py
@@ -17,7 +17,7 @@ from ..json_ld import generate_site_navigation_element_json_ld
 register = template.Library()
 
 
-STATIC_CONTENT_VERSION = 87
+STATIC_CONTENT_VERSION = 88
 
 CLASSIC = 1
 DARCULA = 2
